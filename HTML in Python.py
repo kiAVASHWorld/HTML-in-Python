4a22bd4b8ed24ecc8a4ec0dbde29015670197132
kiAVASHWorld <83844951+kiAVASHWorld@users.noreply.github.com>
2021-10-10 15:11:22 +0330

    Create HTML in Python.py

---
+++ HTML in Python.py
@@ -0,0 +1,17 @@
+#for run html code
+import os
+os.system("touch file.png")
+#no png file only for run html
+#for run html code
+css = """<style></style>"""
+js = """<script></script>"""
+html = """<!DOCTYPE html>
+<html>
+    <head>
+        <title>kiAVASHWorld</title>
+    </head>
+    <body>
+        <a href="https://www.github.com/kiAVASHWorld" target="_blank" style="font-size:35px;">kiAVASHWorld</a>
+    </body>
+</html>"""
+print(f"{html}{js}{css}")
